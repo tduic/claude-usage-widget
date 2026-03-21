70e715c0ffac21ef47db66b754278b8b87048220
Hackpig1974 <me@damon1974.com>
2026-03-21 11:57:34 -0600

    fix: use dynamic import for notarize and scope afterSign to macOS only

--- build/notarize.js
+++ build/notarize.js
@@ -1,11 +1,10 @@
-const { notarize } = require('@electron/notarize');
-
 exports.default = async function notarizing(context) {
   const { electronPlatformName, appOutDir } = context;
   if (electronPlatformName !== 'darwin') {
     return;
   }
 
+  const { notarize } = await import('@electron/notarize');
   const appName = context.packager.appInfo.productFilename;
 
   return await notarize({
